e0c6960ae28a0593ac3ea2e48998c4f263137840
Preben <3rinoth@gmail.com>
2010-12-18 14:48:01 +0100

    the beginning

---
+++ src/com/tap/TheAwesomeProject.java
@@ -0,0 +1,9 @@
+package com.tap;
+
+import com.google.android.maps.MapActivity;
+
+
+public class TheAwesomeProject extends MapActivity {
+    /** Called when the activity is first created. */
+   
+}
